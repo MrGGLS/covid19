60fe33b419025dde7a53db31f23a5f2403c893ef
MrGGLS <2813409972@qq.com>
2021-10-24 16:37:44 +0800

    Initial commit

---
+++ app/src/main/java/com/ggls/covid19/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ggls.covid19;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
