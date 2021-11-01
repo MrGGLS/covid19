b5d83e514d045d94de476a3a7ed10bda032bee47
MrGGLS <2813409972@qq.com>
2021-11-01 16:23:35 +0800

    add a activity

---
+++ app/src/main/java/com/ggls/covid19/DisplayMessageActivity.java
@@ -0,0 +1,48 @@
+package com.ggls.covid19;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    private int mCount=0;
+    private TextView mShowCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(message);
+
+        mShowCount = (TextView)findViewById(R.id.show_count);
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void countUp(View view) {
+        mCount++;
+        if (mShowCount!=null){
+            mShowCount.setText(Integer.toString(mCount));
+        }
+    }
+
+    public void resetToZero(View view) {
+        mCount=0;
+        if(mShowCount!=null){
+            mShowCount.setText(Integer.toString(mCount));
+        }
+    }
+}
