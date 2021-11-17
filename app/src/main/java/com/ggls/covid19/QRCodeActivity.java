711e45505da5ab20ce915720d58956bcbe2bf809
MrGGLS <2813409972@qq.com>
2021-11-18 07:53:35 +0800

    can get location in ORCodeActivity

--- app/src/main/java/com/ggls/covid19/QRCodeActivity.java
+++ app/src/main/java/com/ggls/covid19/QRCodeActivity.java
@@ -1,6 +1,7 @@
 package com.ggls.covid19;
 
 import android.Manifest;
+import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -16,23 +17,44 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
+import com.amap.api.maps.LocationSource;
+import com.amap.api.maps.MapsInitializer;
 import com.huawei.hms.hmsscankit.ScanUtil;
 import com.huawei.hms.hmsscankit.WriterException;
 import com.huawei.hms.ml.scan.HmsBuildBitmapOption;
 import com.huawei.hms.ml.scan.HmsScan;
 import com.huawei.hms.ml.scan.HmsScanAnalyzerOptions;
 
-public class QRCodeActivity extends AppCompatActivity {
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.EasyPermissions;
+
+public class QRCodeActivity extends AppCompatActivity implements AMapLocationListener,LocationSource {
     public static final int DEFAULT_VIEW = 0x22;
     private static final int REQUEST_CODE_SCAN = 0X01;
+    private static final String TAG = "QRCodeActivity";
+    private static final int REQUEST_PERMISSIONS = 7777;
+    public AMapLocationClient mLocationClient = null;
+    public AMapLocationClientOption mLocationOption = null;
+    private LocationSource.OnLocationChangedListener mListener;
+    private String address;
+    private Double latitude;
+    private Double longitude;
+    private Object lock=new Object();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qrcode);
+        MapsInitializer.updatePrivacyAgree(this,true);
+        MapsInitializer.updatePrivacyShow(this,true,true);
         Button scanButton=findViewById(R.id.tap_to_scan);
         scanButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,7 +78,8 @@ public class QRCodeActivity extends AppCompatActivity {
         if (permissions == null || grantResults == null || grantResults.length < 2 || grantResults[0] != PackageManager.PERMISSION_GRANTED || grantResults[1] != PackageManager.PERMISSION_GRANTED) {
             return;
         }
-
+        //设置权限请求结果
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
         if (requestCode == DEFAULT_VIEW) {
             //start ScankitActivity for scanning barcode
             ScanUtil.startScan(QRCodeActivity.this, REQUEST_CODE_SCAN, new HmsScanAnalyzerOptions.Creator().setHmsScanTypes(HmsScan.ALL_SCAN_TYPE).create());
@@ -65,6 +88,10 @@ public class QRCodeActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //                    开启线程获取当前位置
+        showMsg("正在获取当前位置...");
+        initLocation();
+        showMsg("latitude: "+latitude+"longitude: "+longitude);
         //receive result after your activity finished scanning
         super.onActivityResult(requestCode, resultCode, data);
         if (resultCode != RESULT_OK || data == null) {
@@ -132,4 +159,107 @@ public class QRCodeActivity extends AppCompatActivity {
             }
         }
     }
+
+    private void initLocation() {
+        //初始化定位
+        try {
+            mLocationClient = new AMapLocationClient(getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //设置定位回调监听
+        mLocationClient.setLocationListener(this);
+        //初始化AMapLocationClientOption对象
+        mLocationOption = new AMapLocationClientOption();
+        //设置定位模式为AMapLocationMode.Hight_Accuracy，高精度模式。
+        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+        //获取最近3s内精度最高的一次定位结果：
+        //设置setOnceLocationLatest(boolean b)接口为true，启动定位时SDK会返回最近3s内精度最高的一次定位结果。如果设置其为true，setOnceLocation(boolean b)接口也会被设置为true，反之不会，默认为false。
+        mLocationOption.setOnceLocationLatest(true);
+        //设置是否返回地址信息（默认返回地址信息）
+        mLocationOption.setNeedAddress(true);
+        //设置定位请求超时时间，单位是毫秒，默认30000毫秒，建议超时时间不要低于8000毫秒。
+        mLocationOption.setHttpTimeOut(20000);
+        //关闭缓存机制，高精度定位会产生缓存。
+        mLocationOption.setLocationCacheEnable(false);
+        //给定位客户端对象设置定位参数
+        mLocationClient.setLocationOption(mLocationOption);
+        mLocationClient.startLocation();
+    }
+
+    @Override
+    public void onLocationChanged(AMapLocation aMapLocation) {
+        if (aMapLocation != null) {
+            if (aMapLocation.getErrorCode() == 0) {
+                //地址
+                address = aMapLocation.getAddress();
+                latitude = aMapLocation.getLatitude();
+                longitude = aMapLocation.getLongitude();
+                synchronized (mLocationOption){
+                    mLocationOption.notifyAll();
+                }
+                showMsg("当前地理位置是："+address);
+//                只获取一次定位信息，后面的语句可以去掉
+                mLocationClient.stopLocation();
+                if(mListener!=null){
+                    mListener.onLocationChanged(aMapLocation);
+                }
+            } else {
+                Log.e("AmapError", "location Error, ErrCode:"
+                        + aMapLocation.getErrorCode() + ", errInfo:"
+                        + aMapLocation.getErrorInfo());
+            }
+        }
+    }
+
+    public void showMsg(String msg){
+        Toast.makeText(this,msg,Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(null != mLocationClient){
+            mLocationClient.onDestroy();
+        }
+    }
+//  启动定位
+    @Override
+    public void activate(OnLocationChangedListener onLocationChangedListener) {
+        mListener = onLocationChangedListener;
+        mListener = onLocationChangedListener;
+        if (mLocationClient == null) {
+            mLocationClient.startLocation();//启动定位
+        }
+    }
+//    停止定位
+    @Override
+    public void deactivate() {
+        mListener = null;
+        if (mLocationClient != null) {
+            mLocationClient.stopLocation();
+            mLocationClient.onDestroy();
+        }
+        mLocationClient = null;
+    }
+
+    @AfterPermissionGranted(REQUEST_PERMISSIONS)
+    private void requestPermission() {
+        String[] permissions = {
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.READ_PHONE_STATE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+        };
+
+        if (EasyPermissions.hasPermissions(this, permissions)) {
+            //true 有权限 开始定位
+//            showMsg("已获得权限，可以定位啦！");
+            mLocationClient.startLocation();
+        } else {
+            //false 无权限
+            EasyPermissions.requestPermissions(this, "需要权限", REQUEST_PERMISSIONS, permissions);
+        }
+    }
+
 }
